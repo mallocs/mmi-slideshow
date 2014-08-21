7fd0398e09134bc9798e434c6e17f71bb1df1c69
Marcus Ulrich <ulrich@mallocs.net>
2014-08-21 02:36:02 +0200

    Added navigation controls

--- mrslider.js
+++ mrslider.js
@@ -16,20 +16,54 @@ $.widget("mmi.slideshow", {
         carousel: '.carousel',   // Selector for carousel element.
         slides: '.slide',             // Selector for carousel slides.
 
+        controls: true,
+        previousText: '<',    // Use text for slider controls.
+        nextText: '>',           //
+
         // callbacks
         start: null,
         stop: null
     },
-    _create: function() {
+    _create: function() { 
         this.carousel = this.element.children(this.options.carousel);
         this.slides = this.carousel.children(this.options.slides);
+        this.wrapper = this.carousel.wrap('<div style="position:relative;overflow:hidden;"></div>').parent();
+
+        this.carouselWidth = $(this.slides[0]).find("img").width();
+
+        this.wrapper.css({
+            'width': this.carouselWidth + 'px'
+        });
+        this._createControls();
         this.count = this.slides.length;
         this.scrollable = true;
-        this.wrapper = this.carousel.wrap('<div style="position:relative;overflow:hidden;">').parent();
+    },
+
+    _createControls: function() {
+            this.previous = $('<a href="#" class="slides-previous slides-controls" data-slides="previous">' + this.options.previousText + '</a>');
+            this.next = $('<a href="#" class="slides-next slides-controls" data-slides="next">' + this.options.nextText + '</a>');
+            this.wrapper.append(this.next, this.previous);
+    },
+
+    showControls: function() {
+        $(this.wrapper).find(".slides-controls").css({
+            "visibility": "visible"
+        });
+    },
+
+    hideControls: function() {
+        $(this.wrapper).find(".slides-controls").css({
+            "visibility": "hidden"
+        });
     },
 
     _setOption: function( key, value ) {
-         this._super( key, value );
+        this._super( key, value );
+
+        if(key === "controls") {
+            value ? this.showControls() : this.hideControls();
+        }
+
     },
 
     _destroy: function() {
@@ -38,11 +72,7 @@ $.widget("mmi.slideshow", {
 /**
 Functions needed
 
-_createBackground
 _createCaption
-_createControls
-_createNext
-_createPrevious
 _createPaging
 _createPagingFromSprite
 
